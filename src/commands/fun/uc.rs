abfc62966524924cedbdbbdecb18490563da9247
kagaries <archervanhorn@gmail.com>
2025-02-13 02:34:31 -0600

    finished uc commands for now

--- src/commands/fun/uc.rs
+++ src/commands/fun/uc.rs
@@ -1,13 +1,16 @@
-use postgres::{Client, NoTls};
-use serenity::FutureExt;
-use tokio::task;
-use crate::{Error, Context};
+
+use async_postgres::connect;
+use chrono::{Local, NaiveDate};
+use poise::CreateReply;
+use serenity::{all::{CreateEmbed, CreateEmbedFooter}, FutureExt};
+use tokio::{spawn, task};
+use crate::{Error as OtherError, Context};
 use dotenv_codegen::dotenv;
 
 #[poise::command(slash_command, rename = "uc", subcommands("execute", "create", "remove", "get"))]
 pub async fn uc_command(
     ctx: Context<'_>
-) -> Result<(), Error> {
+) -> Result<(), OtherError> {
     Ok(())
 }
 
@@ -15,81 +18,114 @@ pub async fn uc_command(
 pub async fn execute(
     ctx: Context<'_>,
     command: String
-) -> Result<(), Error> { 
-    let result = task::block_in_place(|| {
-        let mut client = Client::connect(
-            &dotenv!("DB_LINK"),
-            NoTls
-        )?;
+) -> Result<(), OtherError> { 
+    let (client, conn) = connect(dotenv!("DB_LINK").parse()?).await?;
 
-        let rows = client.query(&format!("SELECT * FROM commands WHERE name = '{}'", command), &[])?;
+    spawn(conn);
+    
+    let rows = client.query("SELECT * FROM commands WHERE name = $1", &[&command]).await?;
 
-        if !rows.is_empty() {
-            // Return the data to send as a result
-            let to_send: String = rows.get(0).map(|row| row.get::<_, String>(2)).unwrap_or_else(|| String::from("No response found."));
-            Ok(to_send)
-        } else {
-            Err("Command not found.".into())
-        }
-    })?;
+    if let Some(row) = rows.get(0) {
+        let to_send: String = row.get(2);
+        let uses: i32 = row.get(5);
 
-    // Now send the response asynchronously
-    match result {
-        Ok(to_send) => {
-            // Use ctx.say to send the result back
-            ctx.say(to_send).await?;
-        }
-        Err(e) => {
-            // Handle the error by sending an appropriate message
-            ctx.say(format!("Error: {}", e)).await?;
-        }
+        let new_uses: i32 = uses + 1;
+
+        client.execute("UPDATE commands SET uses = $1 WHERE name = $2", &[&&new_uses, &command]).await?;
+
+        ctx.say(&to_send).await?;
+    } else {
+        println!("No results found for command: {}", command);
     }
+
 
     Ok(())
 }
 #[poise::command(slash_command)]
 pub async fn create(
     ctx: Context<'_>,
-    arg: String
-) -> Result<(), Error> { 
-    task::block_in_place(|| {
-        let mut client = Client::connect(
-            &format!("host={} user={} password={}", dotenv!("DB_LINK"), dotenv!("DB_USER"), dotenv!("DB_PASSWORD")),
-            NoTls
-        )?;
+    name: String,
+    sends: String,
+    description: String
+) -> Result<(), OtherError> { 
+    let (client, conn) = connect(dotenv!("DB_LINK").parse()?).await?;
 
-        Ok::<(), Error>(())
-    })?;
+    spawn(conn);
+
+    let rows = client.query("SELECT * FROM commands WHERE name = $1", &[&name]).await?;
+
+    if let Some(row) = rows.get(0) {
+        println!("Command exists!");
+    } else {
+        let now = Local::now();
+        let formatted = now.format("%Y-%m-%d %H:%M:%S").to_string();
+
+        client.execute("INSERT INTO commands (name, author, toSend, description, created, uses) VALUES ($1, $2, $3, $4, $5, 0)", &[&name, &ctx.author().id.get().to_string(), &sends, &description, &formatted]).await?;
+
+        ctx.say(format!("Added Command: {}", name)).await?;
+    }
+
     Ok(())
 }
 #[poise::command(slash_command)]
 pub async fn remove(
     ctx: Context<'_>, 
-    arg: String
-) -> Result<(), Error> {
-    task::block_in_place(|| {
-        let mut client = Client::connect(
-            &format!("host={} user={} password={}", dotenv!("DB_LINK"), dotenv!("DB_USER"), dotenv!("DB_PASSWORD")),
-            NoTls
-        )?;
+    command: String
+) -> Result<(), OtherError> {
 
-        Ok::<(), Error>(())
-    })?;
+    let (client, conn) = connect(dotenv!("DB_LINK").parse()?).await?;
+
+    spawn(conn);
+    
+    let rows = client.query("SELECT * FROM commands WHERE name = $1", &[&command]).await?;
+
+    if let Some(row) = rows.get(0) {
+        let name: String = row.get(0);
+        let author: String = row.get(1);
+
+        if (ctx.author().id.get().to_string().contains(&author)) {
+            client.execute("DELETE FROM commands WHERE name = $1", &[&command]).await?;
+
+            ctx.say(format!("Deleted: {}", name)).await?;
+        } else {
+            ctx.say("You do not own this command.").await?;
+        }
+    } else {
+        println!("No results found for command: {}", command);
+    }
+
     Ok(()) 
 }
 #[poise::command(slash_command)]
 pub async fn get(
     ctx: Context<'_>, 
-    arg: String
-) -> Result<(), Error> {
-    task::block_in_place(|| {
-        let mut client = Client::connect(
-            &format!("host={} user={} password={}", dotenv!("DB_LINK"), dotenv!("DB_USER"), dotenv!("DB_PASSWORD")),
-            NoTls
-        )?;
+    command: String
+) -> Result<(), OtherError> {
 
-        Ok::<(), Error>(())
-    })?;
+    let (client, conn) = connect(dotenv!("DB_LINK").parse()?).await?;
+
+    spawn(conn);
+    
+    let rows = client.query("SELECT * FROM commands WHERE name = $1", &[&command]).await?;
+
+    if let Some(row) = rows.get(0) {
+        let name: String = row.get(0);
+        let author: String = row.get(1);
+        let to_send: String = row.get(2);
+        let description: String = row.get(3);
+        let created: String = row.get(4);
+        let uses: i32 = row.get(5);
+
+        let embed = CreateEmbed::new().title(name).description(description)
+        .field("Sends:", to_send, true)
+        .field("Author:", format!("<@{}>", author), true)
+        .footer(CreateEmbedFooter::new(format!("Created: {} || Uses: {}", created, uses.to_string())));
+
+        ctx.send(CreateReply::default().embed(embed)).await?;
+    } else {
+        println!("No results found for command: {}", command);
+    }
+
     Ok(()) 
 }
 
